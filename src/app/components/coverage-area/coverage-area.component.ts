b9f89c86cd91274fd97f75fca4152bd259b0ed83
Matheus Azevedo <matheusoliveiraazevedo@gmail.com>
2025-03-31 11:11:26 -0300

    Inclusão de div bloqueando o zoom do mapa em area de cobertura

--- src/app/components/coverage-area/coverage-area.component.ts
+++ src/app/components/coverage-area/coverage-area.component.ts
@@ -1,14 +1,16 @@
-import { Component, HostBinding } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostBinding, TemplateRef, ViewChild, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-coverage-area',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './coverage-area.component.html',
   styleUrl: './coverage-area.component.scss'
 })
 export class CoverageAreaComponent {
 
   @HostBinding('class') class = 'd-flex justify-content-center'
+  showMap: boolean = false
 
 }
